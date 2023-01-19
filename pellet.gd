369afe683ea9ca383fbda54f722cfc6b60b6b5c1
Ty Gutowski <tygutowski@gmail.com>
2023-01-18 20:57:51 -0500

    fixed sprites, fixed shotgun

--- pellet.gd
+++ pellet.gd
@@ -10,3 +10,7 @@ func _on_area_entered(area):
 	if area.is_in_group("enemy_hitbox"):
 		area.get_parent().take_damage(damage) # enemy takes damage
 		queue_free()
+
+func _on_body_entered(body):
+	if body.is_in_group("tilemap"):
+		queue_free()
--- shotgun.gd
+++ shotgun.gd
@@ -5,15 +5,19 @@ var ammo = max_ammo
 var shooting = false
 
 var spread = 10.0 # in degrees
-var pellets = 10
-var projectile_speed = 1.0
-var speed_randomness = 1.0
+var pellets = 4
+var projectile_speed = 5.0
+var speed_randomness = 0.5
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var pellet_scene = preload("res://pellet.tscn") 
 @onready var level = get_tree().get_first_node_in_group("level")
 var frames_between_shots
-var frames_since_last_shot = 0
+var frames_since_last_shot
+
+func _ready():
+	update_stats()
+	frames_since_last_shot = frames_between_shots
 
 func start_using():
 	update_stats()
@@ -23,7 +27,7 @@ func stop_using():
 	shooting = false
 
 func _process(_delta):
-	rotation = get_angle_to(get_global_mouse_position())
+	rotation += get_angle_to(get_global_mouse_position())
 	frames_since_last_shot += 1
 	if shooting && frames_since_last_shot >= frames_between_shots:
 		print("shoot")
@@ -33,11 +37,13 @@ func _process(_delta):
 
 func instance_pellet():
 	var projectile = pellet_scene.instantiate()
-	projectile.rotation = rotation + randf_range(-spread/2, spread/2)
-	projectile.global_position = $Barrel.global_position
-	projectile.velocity = Vector2(randf_range(projectile_speed-speed_randomness/2, projectile_speed+speed_randomness/2),0)
+	#projectile.rotation = rotation + randf_range(-spread/2, spread/2)
+	projectile.global_position = $Sprite2D/Barrel.global_position
+	var vector_to_mouse = (get_global_mouse_position() - global_position).normalized()
+	vector_to_mouse = vector_to_mouse.rotated(deg_to_rad(randf_range(-spread/2, spread/2)))
+	var speed = randf_range(projectile_speed-speed_randomness/2, projectile_speed+speed_randomness/2)
+	projectile.velocity = vector_to_mouse * speed
 	level.add_child(projectile)
 
 func update_stats():
 	frames_between_shots = floor(clamp(60.0 / player.base_attack_speed * player.attack_speed_modifier, 1, INF))
-	frames_since_last_shot = frames_between_shots
